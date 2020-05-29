9262d344fa6bf032c36421bde055e22f9dddd484
kylo <2513084541@qq.com>
2020-05-29 20:15:09 +0800

    temp commit

--- source/1.getting_started/hello_texture.cpp
+++ source/1.getting_started/hello_texture.cpp
@@ -1,11 +1,16 @@
+#pragma once
 #include <iostream>
 #include <string>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb_image.h>
+
 
 #include "util/shader.h"
 #include "util/commonUtil.h"
+#include "util/filesystem.h"
+
 
 
 
@@ -84,6 +89,17 @@ int main()
 	glEnableVertexAttribArray(2);
 
 
+
+	// load and create texture
+	unsigned int texture1, texture2;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load(FileSystem::getPath("resources/textures/awesomeface.png").c_str(), &width, &height, &nrChannels, 0);
 
 
 
