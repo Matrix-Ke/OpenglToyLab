dec5f6ebd25ea709acf203059aae9ea76e4baafa
kylo <2513084541@qq.com>
2020-06-08 09:03:34 +0800

    add operation

---
+++ source/oper/operation.cpp
@@ -0,0 +1,29 @@
+#include "util/operation.h"
+
+using namespace Oper;
+using namespace std;
+
+
+Oper::Operation::Operation(bool IsHold /*= true*/): m_isHold(IsHold) {};
+
+Oper::Operation::~Operation() {};
+
+bool Oper::Operation::IsHold()
+{
+	return m_isHold;
+}
+
+void Oper::Operation::SetIsHold(bool ishold)
+{
+	this->m_isHold = ishold;
+}
+
+void Oper::Operation::operator()()
+{
+	this->run();
+}
+
+void Oper::Operation::ProtectDelete(Operation* op)
+{
+	delete op;
+}
